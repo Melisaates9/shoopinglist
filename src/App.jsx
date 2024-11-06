39b6fb20c9c3b4ad689f96514a68724300c4d401
melisa serap ates <melisaserap@gmail.com>
2024-11-06 16:26:37 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const shops = ["Gratis", "Mac", "misha"];
+const katagori=['Makyaj', 'Cilt Bakım','Saç Bakım','Parfüm & Deodorant']
+function App() {
+  return ( 
+  <>
+
+
+  </>
+  )
+}
+
+export default App;
